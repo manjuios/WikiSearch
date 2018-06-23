039efd1efafb8fb19b76d61cdfc54798991ec1ff
Manjunath Naragund <manunath.naragund@mantralabsglobal.com>
2018-06-23 14:51:04 +0530

    Commit Xcode project

---
+++ WikiSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiSearch
+//
+//  Created by Manjunath Naragund on 23/06/18.
+//  Copyright © 2018 Manjunath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
